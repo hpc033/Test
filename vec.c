a43f62f02582b0ae443b3ee90c3f4757d3c52605
hpc033 <1583123374@qq.com>
2017-12-03 16:30:53 +0800

    vec.c

---
+++ vec.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <omp.h>
+
+int main()
+
+{
+
+    clock_t t1,t2;  
+
+    t1 = clock();   
+
+    int i, n;
+
+    float a[100], b[100], result;
+
+
+    n = 100;
+
+    result = 0.0;
+
+    for (i = 0; i < n; i++)
+
+    {
+
+        a[i] = i * 1.0;
+
+        b[i] = i * 2.0;
+
+        
+
+    }
+
+    #pragma omp parallel for private(i) reduction(+:result)
+
+    for (i = 0; i < n; i++)
+
+    {
+
+        
+
+        result = result + (a[i] * b[i]);
+
+    }
+
+    t2 = clock();  
+
+    printf("t=%f\n",(double)(t2-t1)/CLOCKS_PER_SEC); 
+
+    printf("a*b= %12.6lf\n", result);
+
+    
+
+    return 0;
+
+}
